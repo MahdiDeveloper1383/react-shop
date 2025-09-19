18123832ee9ef7c734b1ccdba6cd450b27f4e25b
Mahdi <mahd8767trfx@gmail.com>
2025-09-19 14:22:35 +0330

    add links for footer

--- src/Components/Shop/footer.tsx
+++ src/Components/Shop/footer.tsx
@@ -1,31 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const Footer = () => {
+    const navigate = useNavigate()
     return ( 
         <footer className="flex flex-col space-y-10 justify-center w-full bg-gray-100 px-10 py-10">
 
     <nav className="flex justify-center flex-wrap gap-6 text-gray-500 font-medium">
-        <a className="hover:text-gray-900" href="#">Home</a>
-        <a className="hover:text-gray-900" href="#">About</a>
-        <a className="hover:text-gray-900" href="#">Services</a>
-        <a className="hover:text-gray-900" href="#">Media</a>
-        <a className="hover:text-gray-900" href="#">Gallery</a>
-        <a className="hover:text-gray-900" href="#">Contact</a>
+        <a className="hover:text-gray-900 cursor-pointer" onClick={()=>navigate('/')}>Home</a>
+        <a className="hover:text-gray-900 cursor-pointer" onClick={()=>navigate('/about-us')}>About</a>
+        <a className="hover:text-gray-900 cursor-pointer" onClick={()=>navigate('/contact-us')}>Contact</a>
     </nav>
 
     <div className="flex justify-center space-x-5">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+        <a href="https://github.com/MahdiDeveloper1383" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/color/github" width={40} />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/mahdi-mohamad-nejad-3013a5351" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
-        </a>
-        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
         </a>
     </div>
     <p className="text-center text-gray-700 font-medium">&copy; 2022 Company Ltd. All rights reservered.</p>
